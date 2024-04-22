2a52ab6621f54c124a87997465d7e4f8f9693ba3
Alexandre Ribeiro <alexandreribeiro0209@gmail.com>
2024-04-22 15:16:45 -0300

    Update main

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -23,16 +23,16 @@ class Main {
     $cabecalho = ['ID', 'Title', 'Type', 'Author 1', 'Author 1 Institution', 'Author 2', 'Author 2 Institution', 'Author 3', 'Author 3 Institution', 'Author 4', 'Author 4 Institution', 'Author 5', 'Author 5 Institution', 'Author 6', 'Author 6 Institution', 'Author 7', 'Author 7 Institution', 'Author 8', 'Author 8 Institution', 'Author 9', 'Author 9 Institution','Author 10', 'Author 10 Institution'];
 
     $rowFromValues = WriterEntityFactory::createRowFromArray($cabecalho);
-    $writer->addRow($rowFromValues); 
+    $writer->addRow($rowFromValues);
 
     $dom = new \DOMDocument('1.0', 'utf-8');
     $html = file_get_contents(__DIR__ . '/../../assets/origin.html');
 
     @$dom->loadHTML($html);
 
-    $data = (new Scrapper())->scrap($dom);    
+    $data = (new Scrapper())->scrap($dom);
 
-    $i=0;
+    $i = 0;
     foreach ($data as $item) {
       $result = [];
       foreach ($item['authors'] as $author) {
@@ -41,22 +41,23 @@ class Main {
         $result[$i] = $author['institution'];
         $i++;
       }
-      $i=0;
+      $i = 0;
       $item['authors'] = $result;
 
       $result = [];
       foreach ($item as $key => $value) {
-          if ($key === 'authors') {
-              foreach ($value as $author) {
-                $result[] = $author;
+              if ($key === 'authors') {
+                  foreach ($value as $author) {
+                      $result[] = $author;
+                  }
+              } else {
+                  $result[] = $value;
               }
-          } else {
-              $result[] = $value;
-          }
       }
       $insert = WriterEntityFactory::createRowFromArray($result);
       $writer->addRow($insert);
     }
     $writer->close();
+    
   }
 }
