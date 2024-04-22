ba6a4aa04f2ca469131482daf6063fbf61e7c5b8
Alexandre Ribeiro <alexandreribeiro0209@gmail.com>
2024-04-22 15:31:17 -0300

    Update main

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -20,7 +20,11 @@ class Main {
     $writer = WriterEntityFactory::createXLSXWriter();
     $writer->openToBrowser($file);
 
-    $cabecalho = ['ID', 'Title', 'Type', 'Author 1', 'Author 1 Institution', 'Author 2', 'Author 2 Institution', 'Author 3', 'Author 3 Institution', 'Author 4', 'Author 4 Institution', 'Author 5', 'Author 5 Institution', 'Author 6', 'Author 6 Institution', 'Author 7', 'Author 7 Institution', 'Author 8', 'Author 8 Institution', 'Author 9', 'Author 9 Institution', 'Author 10', 'Author 10 Institution'];
+    $cabecalho = ['ID', 'TITLE', 'TYPE'];
+    for ($i = 1; $i <= 10; $i++) { 
+      $cabecalho [] = 'Author '.$i;
+      $cabecalho [] = 'Author instituition '.$i;
+    }
 
     $rowFromValues = WriterEntityFactory::createRowFromArray($cabecalho);
     $writer->addRow($rowFromValues);
