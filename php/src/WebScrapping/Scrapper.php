bb80aaf8923b07e731c280a0f42160b673627698
Alexandre Ribeiro <alexandreribeiro0209@gmail.com>
2024-04-22 15:37:29 -0300

    Update Scrapper

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -16,34 +16,28 @@ class Scrapper {
   /**
    * Loads paper information from the HTML and returns the array with the data.
    */
+
   public function scrap(\DOMDocument $dom): array{
 
-    // Declaracao de objeto
     $xpath = new \DOMXPath($dom);
 
-    // Captura a div com a class especificada
     $paperCard = $xpath->query("//a[contains(@class, 'paper-card')]");
     $count = 0;
-
-    $informacoes = array();
+    $informacoes = [];
 
     foreach ($paperCard as $papel) {
-      // Capturar a div com classe "volume-info"
       $id = $xpath->query("//div[contains(@class, 'volume-info')]")->item($count);
       $id = $id->nodeValue;
 
-      // Capturar a h4 com classe "paper-title"
       $title = $xpath->query("//h4[contains(@class, 'paper-title')]")->item($count);
       $title = $title->nodeValue;
 
-      // Capturar a div com classe "tags mr-sm"
       $type = $xpath->query("//div[contains(@class, 'tags mr-sm')]")->item($count);
       $type = $type->nodeValue;
 
-      // Capturar a span dentro da div 'authors'
       $span = $papel->getElementsByTagName('span');
 
-      $authores = array();
+      $authores = [];
 
       foreach ($span as $author) {
         $name = $author->nodeValue;
@@ -52,8 +46,10 @@ class Scrapper {
       }
       $count++;
 
-      $informacoes [] = get_object_vars(new Paper($id, $title, $type, $authores));
+      $informacoes[] = get_object_vars(new Paper($id, $title, $type, $authores));
     }
+    
     return $informacoes;
   }     
+
 }
