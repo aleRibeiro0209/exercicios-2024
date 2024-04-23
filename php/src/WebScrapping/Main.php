6ebc5d765b473b9c03b02471487596a9b52b4a70
Alexandre Ribeiro <alexandreribeiro0209@gmail.com>
2024-04-23 16:48:05 -0300

    Run test actions

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -4,7 +4,7 @@ namespace Chuva\Php\WebScrapping;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-use \Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
 
 /**
  * Runner for the Webscrapping exercice.
