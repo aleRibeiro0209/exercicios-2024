90531625218b6287d3fa9565777142b4c1d0667c
Alexandre Ribeiro <alexandreribeiro0209@gmail.com>
2024-04-23 15:37:27 -0300

    Update fork

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -2,6 +2,10 @@
 
 namespace Chuva\Php\WebScrapping;
 
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+
 /**
  * Runner for the Webscrapping exercice.
  */
@@ -11,13 +15,56 @@ class Main {
    * Main runner, instantiates a Scrapper and runs.
    */
   public static function run(): void {
+    $file = 'planilhaProceedings.xlsx';
+    $writer = WriterEntityFactory::createXLSXWriter();
+    $writer->openToBrowser($file);
+
+    $cabecalho = ['ID', 'TITLE', 'TYPE'];
+    for ($i = 1; $i <= 10; $i++) {
+      $cabecalho[] = 'Author ' . $i;
+      $cabecalho[] = 'Author instituition ' . $i;
+    }
+
+    $rowFromValues = WriterEntityFactory::createRowFromArray($cabecalho);
+    $writer->addRow($rowFromValues);
+
     $dom = new \DOMDocument('1.0', 'utf-8');
-    $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
+    $html = file_get_contents(__DIR__ . '/../../assets/origin.html');
+
+    @$dom->loadHTML($html);
 
     $data = (new Scrapper())->scrap($dom);
 
-    // Write your logic to save the output file bellow.
-    print_r($data);
+    $i = 0;
+    foreach ($data as $item) {
+      $result = [];
+      foreach ($item['authors'] as $author) {
+        $result[$i] = $author['name'];
+        $i++;
+        $result[$i] = $author['institution'];
+        $i++;
+      }
+      $i = 0;
+      $item['authors'] = $result;
+
+      $result = [];
+      foreach ($item as $key => $value) {
+        if ($key === 'authors') {
+          foreach ($value as $author) {
+            $result[] = $author;
+          }
+        }
+        else {
+          $result[] = $value;
+        }
+      }
+      $insert = WriterEntityFactory::createRowFromArray($result);
+      $writer->addRow($insert);
+    }
+    $writer->close();
+
   }
 
 }
+
+(new Main())->run();
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -14,17 +14,38 @@ class Scrapper {
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
+
+    $xpath = new \DOMXPath($dom);
+
+    $paperCard = $xpath->query("//a[contains(@class, 'paper-card')]");
+    $count = 0;
+    $informacoes = [];
+
+    foreach ($paperCard as $papel) {
+      $id = $xpath->query("//div[contains(@class, 'volume-info')]")->item($count);
+      $id = $id->nodeValue;
+
+      $title = $xpath->query("//h4[contains(@class, 'paper-title')]")->item($count);
+      $title = $title->nodeValue;
+
+      $type = $xpath->query("//div[contains(@class, 'tags mr-sm')]")->item($count);
+      $type = $type->nodeValue;
+
+      $span = $papel->getElementsByTagName('span');
+
+      $authores = [];
+
+      foreach ($span as $author) {
+        $name = $author->nodeValue;
+        $institution = $author->getAttribute('title');
+        $authores[] = get_object_vars(new Person($name, $institution));
+      }
+      $count++;
+
+      $informacoes[] = get_object_vars(new Paper($id, $title, $type, $authores));
+    }
+
+    return $informacoes;
   }
 
 }
